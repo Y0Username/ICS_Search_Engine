1ade9af79c9e5e2c0ea61b82cdcaef0a97c0ce9e
Vivek <vivekpn.93@gmail.com>
2017-03-11 02:53:51 -0800

    TF-IDF Calculation

--- src/main/java/com/se/index/TfIdf.java
+++ src/main/java/com/se/index/TfIdf.java
@@ -4,10 +4,16 @@ public class TfIdf {
 	private static Long N = 37419l;
 
 	public static double termFrequency(Integer termFrequency) {
+		if(termFrequency == 0){
+			return 0;
+		}
 		return (1 + Math.log10(termFrequency));
 	}
 
 	public static double inverseDocFrequency(Integer docFrequency) {
+		if(docFrequency == 0){
+			return 0;
+		}
 		return Math.log10(N.doubleValue() / docFrequency);
 	}
 
