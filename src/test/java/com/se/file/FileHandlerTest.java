64b68db44b34066b59834db72e6f5a17f5d2791c
Vivek <vivekpn.93@gmail.com>
2017-03-16 21:30:03 -0700

    Uncommented the tests instead skipping them.

--- src/test/java/com/se/file/FileHandlerTest.java
+++ src/test/java/com/se/file/FileHandlerTest.java
@@ -1,13 +1,14 @@
-//package com.se.file;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//public class FileHandlerTest {
-//	
-//	@Test
-//	public void fetchesSnippet() {
-//		Assert.assertEquals("Ihler Associate Professor ",FileHandler.fetch(3, 5, "64/443", "www.ics.uci.edu/~ihler"));
-//	}
-//
-//}
+package com.se.file;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class FileHandlerTest {
+	
+	@Ignore @Test
+	public void fetchesSnippet() {
+		Assert.assertEquals("Ihler Associate Professor ",FileHandler.fetch(3, 5, "64/443", "www.ics.uci.edu/~ihler"));
+	}
+
+}
