97089f6813a235a7c061e575d6bf786d43b398c2
Yathish <yathishsg@gmail.com>
2017-03-18 02:28:14 -0700

    NDCG refactor

--- src/main/java/com/se/util/NDCG.java
+++ src/main/java/com/se/util/NDCG.java
@@ -14,9 +14,18 @@ public class NDCG {
         String query = "mondego";
         Map<String, Double> googleMap = FileHandler.relevancyReader(query);
         List<Double> googleList = new ArrayList<>(googleMap.values());
+
         QueryRunner queryRunner = new QueryRunner();
-        Map<String, Double> chotheMap = new LinkedHashMap<>();
         List<SearchResult> SearchResults = queryRunner.search(query);
+        List<Double> chotheList = mapNDCG(query, SearchResults, googleMap);
+
+        List<Double> ndcg = calculateNDCG(googleList, chotheList);
+        System.out.println(ndcg);
+    }
+
+    public static List<Double> mapNDCG(String query, List<SearchResult> SearchResults, Map<String, Double> googleMap){
+        Map<String, Double> chotheMap = new LinkedHashMap<>();
+
         if(query.equals("information retrieval")){
             SearchResults = SearchResults.subList(0, 5);
         }
@@ -28,8 +37,11 @@ public class NDCG {
                 chotheMap.put(result.getUrl(), 0.0);
             }
         }
-
         List<Double> chotheList = new ArrayList<>(chotheMap.values());
+        return chotheList;
+    }
+
+    public static List<Double> calculateNDCG(List<Double> googleList, List<Double> chotheList){
         for(int i = 1; i < googleList.size(); i++){
             googleList.set(i, googleList.get(i) / (Math.log10(i+1) / Math.log10(2)));
             googleList.set(i, (googleList.get(i) + googleList.get(i-1)));
@@ -40,6 +52,7 @@ public class NDCG {
         for(int i = 0; i < googleList.size(); i++){
             ndcg.add(chotheList.get(i) / googleList.get(i));
         }
-        System.out.println(ndcg);
+        return ndcg;
     }
+
 }
